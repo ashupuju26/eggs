6afddc930818e7aff5558f5e42a5e0fe32d57636
ashupuju26 <137829652+ashupuju26@users.noreply.github.com>
2023-10-09 22:16:31 +0400

    Add files via upload

---
+++ restaurant website/main.js
@@ -0,0 +1,9 @@
+var typed = new Typed('.typing', {
+    strings: ['UAE(JBR, City Walk)',
+     'Egypt', 'Qatar', 'Canada','Pakistan'], 
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop: true
+
+})
